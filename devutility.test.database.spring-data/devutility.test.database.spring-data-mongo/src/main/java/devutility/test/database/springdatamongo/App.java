61a36968e98e652ba2990574c9cc367088062003
Aldwin Su <fangyansu@163.com>
2018-08-23 17:10:56 +0800

    Update FindOneTest

---
+++ devutility.test.database.spring-data/devutility.test.database.spring-data-mongo/src/main/java/devutility/test/database/springdatamongo/App.java
@@ -0,0 +1,7 @@
+package devutility.test.database.springdatamongo;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
--- devutility.test.database.spring-data/devutility.test.database.spring-data-mongo/src/test/java/devutility/test/database/springdatamongo/mongotemplate/FindOneTest.java
+++ devutility.test.database.spring-data/devutility.test.database.spring-data-mongo/src/test/java/devutility/test/database/springdatamongo/mongotemplate/FindOneTest.java
@@ -4,25 +4,25 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
+import com.devutility.test.entity.mongo.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import devutility.internal.test.TestExecutor;
 import devutility.test.database.springdatamongo.BaseTest;
-import devutility.test.database.springdatamongo.entity.GlobalAccount;
 
 public class FindOneTest extends BaseTest {
 	@Override
 	public void run() {
 		MongoOperations mongoOperations = (MongoOperations) applicationContext.getBean("mongoTemplate");
 		Query query = new Query();
-		query.addCriteria(Criteria.where("ID").is("GAG001ORCL"));
+		query.addCriteria(Criteria.where("name").is("Aldwin"));
 
-		GlobalAccount globalAccount = mongoOperations.findOne(query, GlobalAccount.class);
+		User user = mongoOperations.findOne(query, User.class);
 		ObjectMapper objectMapper = new ObjectMapper();
 
 		try {
-			System.out.println(objectMapper.writeValueAsString(globalAccount));
+			System.out.println(objectMapper.writeValueAsString(user));
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
