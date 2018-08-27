baa2825da6f41bc00b0792158d1f32158c06830b
Aldwin Su <fangyansu@163.com>
2018-08-27 15:15:36 +0800

    Update

--- devutility.test.database.spring-data/devutility.test.database.spring-data-jpa/src/main/java/devutility/test/database/springdatajpa/config/DataSource2Configuration.java
+++ devutility.test.database.spring-data/devutility.test.database.spring-data-jpa/src/main/java/devutility/test/database/springdatajpa/config/DataSource2Configuration.java
@@ -7,7 +7,7 @@ import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -15,7 +15,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
-@Configuration
+//@Configuration
 @PropertySource("classpath:db.properties")
 @EnableJpaRepositories(basePackages = "devutility.test.database.springdatajpa.dao.mysql", entityManagerFactoryRef = "entityManagerFactory2", transactionManagerRef = "transactionManager2")
 public class DataSource2Configuration {
