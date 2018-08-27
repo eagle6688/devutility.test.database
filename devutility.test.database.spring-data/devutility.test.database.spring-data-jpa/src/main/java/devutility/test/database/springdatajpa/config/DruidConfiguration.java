3b1a131f221d846999e2ebe50c06ebc1cb790c50
Aldwin Su <fangyansu@163.com>
2018-08-27 18:19:46 +0800

    Update

--- devutility.test.database.spring-data/devutility.test.database.spring-data-jpa/src/main/java/devutility/test/database/springdatajpa/config/DruidConfiguration.java
+++ devutility.test.database.spring-data/devutility.test.database.spring-data-jpa/src/main/java/devutility/test/database/springdatajpa/config/DruidConfiguration.java
@@ -1,19 +1,30 @@
 package devutility.test.database.springdatajpa.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 
 @Configuration
 public class DruidConfiguration {
 	@Bean
 	public ServletRegistrationBean<StatViewServlet> druidStatViewServlet() {
-		ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<StatViewServlet>(new StatViewServlet(), "/druid/*");
+		ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
 		servletRegistrationBean.addInitParameter("loginUsername", "admin");
 		servletRegistrationBean.addInitParameter("loginPassword", "admin");
 		servletRegistrationBean.addInitParameter("resetEnable", "false");
 		return servletRegistrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean<WebStatFilter> druidStatFilter() {
+		FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<>(new WebStatFilter());
+		filterRegistrationBean.setName("DruidWebStatFilter");
+		filterRegistrationBean.addUrlPatterns("/*");
+		filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+		return filterRegistrationBean;
 	}
 }
--- devutility.test.database.spring-data/devutility.test.database.spring-data-jpa/src/main/java/devutility/test/database/springdatajpa/controller/DruidController.java
+++ devutility.test.database.spring-data/devutility.test.database.spring-data-jpa/src/main/java/devutility/test/database/springdatajpa/controller/DruidController.java
@@ -1,10 +1,16 @@
 package devutility.test.database.springdatajpa.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.druid.stat.DruidStatManagerFacade;
 
 @RestController
 @RequestMapping("/druid")
 public class DruidController {
-
+	@GetMapping("/stat")
+	public Object stat() {
+		return DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
+	}
 }
