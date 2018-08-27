3c9bb357c4f81bd6c63ac56deea8ae6960b1f4b3
Aldwin Su <fangyansu@163.com>
2018-08-27 18:24:35 +0800

    Update /druid-stat

--- devutility.test.database.spring-data/devutility.test.database.spring-data-jpa/src/main/java/devutility/test/database/springdatajpa/controller/DruidController.java
+++ devutility.test.database.spring-data/devutility.test.database.spring-data-jpa/src/main/java/devutility/test/database/springdatajpa/controller/DruidController.java
@@ -1,15 +1,13 @@
 package devutility.test.database.springdatajpa.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.druid.stat.DruidStatManagerFacade;
 
 @RestController
-@RequestMapping("/druid")
 public class DruidController {
-	@GetMapping("/stat")
+	@GetMapping("/druid-stat")
 	public Object stat() {
 		return DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
 	}
