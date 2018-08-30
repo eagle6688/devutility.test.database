d13df756c593e0c9216ab5cf7c382ce1a94f1f4b
Aldwin Su <fangyansu@163.com>
2018-08-30 18:24:15 +0800

    Update

---
+++ devutility.test.database.mybatis/devutility.test.database.mybatis-springboot/src/main/java/devutility/test/database/mybatis/springboot/DUTDB_MyBatis_SpringBootApp.java
@@ -0,0 +1,11 @@
+package devutility.test.database.mybatis.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DUTDB_MyBatis_SpringBootApp {
+	public static void main(String[] args) {
+		SpringApplication.run(DUTDB_MyBatis_SpringBootApp.class, args);
+	}
+}
