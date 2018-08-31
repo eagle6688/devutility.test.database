f3da32071106803ee94952882b968701685dbadd
Aldwin Su <fangyansu@163.com>
2018-08-31 18:14:57 +0800

    Update

--- devutility.test.database.mybatis/devutility.test.database.mybatis-springboot/src/main/java/devutility/test/database/mybatis/springboot/config/DataSource1Configuration.java
+++ devutility.test.database.mybatis/devutility.test.database.mybatis-springboot/src/main/java/devutility/test/database/mybatis/springboot/config/DataSource1Configuration.java
@@ -1,5 +1,7 @@
 package devutility.test.database.mybatis.springboot.config;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -7,28 +9,41 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 
 @Configuration
 @PropertySource("classpath:db.properties")
-@MapperScan(basePackages = { "devutility.test.database.mybatis.springboot.ds1.mappers" }, sqlSessionFactoryRef = "sqlSessionFactory1", sqlSessionTemplateRef = "sqlSessionTemplate1")
+@MapperScan(basePackages = { "devutility.test.database.mybatis.springboot.ds1" }, sqlSessionFactoryRef = "sqlSessionFactory1", sqlSessionTemplateRef = "sqlSessionTemplate1")
 public class DataSource1Configuration {
 	@Primary
 	@Bean
 	@ConfigurationProperties("db1.sqlserver")
 	public DataSource dataSource1() {
-		return DataSourceBuilder.create().build();
+		return DruidDataSourceBuilder.create().build();
 	}
 
 	@Bean
-	public SqlSessionFactory sqlSessionFactory1(DataSource dataSource1) throws Exception {
+	@ConfigurationProperties("db1.sqlserver")
+	public Properties ormProperties1() {
+		return new Properties();
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory1(DataSource dataSource1, Properties ormProperties1) throws Exception {
+		String configLocation = ormProperties1.getProperty("mybatis.config-location");
+		Resource[] resources = new PathMatchingResourcePatternResolver().getResources(configLocation);
+
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource1);
+		sqlSessionFactoryBean.setConfigLocation(resources[0]);
 		return sqlSessionFactoryBean.getObject();
 	}
 
